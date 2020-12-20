28bc079c05784b135c2032d4edcd98a7b9b1a8b2
RezaMpl <52689106+RezaMpl@users.noreply.github.com>
2020-12-20 08:23:49 +0330

    Create MathClass.cs

---
+++ MathClass.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextWizard
+{
+    class MathClass
+    {
+        private void MathMethod(int value1, decimal value2)
+        {
+            Math.Round(10.666667866, value1);
+            Math.Sqrt(50.90);
+            Math.Abs(-857);
+            Math.Ceiling(865.98);
+            Math.Cos(270);
+            Math.Cosh(234.324);
+            Math.Max(20, 100);
+            Math.Sin(180);
+            Math.Pow(2, 18);
+            double f = Math.PI;
+            int result = 0;
+            Math.DivRem(57777, 78, out result);
+            Math.Floor(54.7657);
+            Math.Log(100);
+            Math.Min(value1, Math.Acos(0.2343232));
+            Math.Acosh(76.89879);
+            Math.Asinh(54.876);
+            Math.Atan(876.89);
+            Math.Atan2(34.234, 23.23);
+            Math.Atanh(48.34);
+            Math.BigMul(12, 120);
+            Math.BitDecrement(12.12);
+            Math.BitIncrement(12.14);
+            Math.Cbrt(23.123);
+            Math.Clamp(12, 4334, 3444);
+            Math.CopySign(23.432, 11.99);
+            Math.Equals(12, 12);
+            Math.Exp(55.99);
+            Math.FusedMultiplyAdd(24.1, 25.7, 26.8);
+            Math.IEEERemainder(88.8, 44.4);
+            Math.ILogB(4.666666);
+            Math.Log(5.0001);
+            Math.Log10(5.0001);
+            Math.Log2(5.0001);
+            Math.MaxMagnitude(5.0001, 52.0001);
+            Math.MinMagnitude(88.8, 44.4);
+            Math.ReferenceEquals(88.8, 44.4);
+            Math.ScaleB(88.8, 44);
+            Math.Sign(88);
+            Math.Sinh(88.8);
+            Math.Tanh(88.8);
+            Math.Truncate(44.4);
+            double e = Math.E;
+            double sum = e + f;
+            // Methods of Math class...^
+        }}}
