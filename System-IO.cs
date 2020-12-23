66dad13652a38972978d4370d5ba7b8898f5a4de
RezaMpl <52689106+RezaMpl@users.noreply.github.com>
2020-12-23 09:02:30 +0330

    Create basic code

--- System-IO.cs
+++ System-IO.cs
@@ -1,1 +1,14 @@
 
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace TextWizard
+{
+    class MathClass
+    {
+    private void IOMethod(int value1)
+        {
+        
+        }}}
