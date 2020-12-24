2f09a900f6f3e8b24d857497705dba29920124ff
RezaMp <52689106+RezaMpx@users.noreply.github.com>
2020-12-24 11:31:27 +0330

    Create IO...

---
+++ System-IO-2.cs
@@ -0,0 +1,34 @@
+namespace IO
+{
+    class IoClass
+    {
+        public void IoMethod()
+        {
+            /*
+            Exception exception1 = new Exception();
+            Exception exception2 = new Exception("you are midwinner");
+            Exception exception3 = new Exception("you are midwinner", exception2);
+            FileLoadException fileLoadExc1 = new FileLoadException();
+            FileLoadException fileLoadExc2 = new FileLoadException("you are winner");
+            FileLoadException fileLoadExc3 = new FileLoadException("you are winner", exception3);
+            FileLoadException fileLoadExc4 = new FileLoadException("you are winner", "YAW");
+            FileLoadException fileLoadExc5 = new FileLoadException("you are winner", "YAW", exception3);
+            string f1 = fileLoadExc1.Data
+            string f1 = fileLoadExc1.Equals
+            string f1 = fileLoadExc1.FileName;
+            string f2 = fileLoadExc1.FusionLog;
+            string f2 = fileLoadExc1.GetBaseException
+            string f2 = fileLoadExc1.GetHashCode
+            string f2 = fileLoadExc1.GetType
+            string f2 = fileLoadExc1.InnerException
+            string f2 = fileLoadExc1.Source
+            string f2 = fileLoadExc1.StackTrace
+            string f2 = fileLoadExc1.TargetSite
+            string f2 = fileLoadExc1.ToString
+            string f3 = fileLoadExc1.Message;
+                      fileLoadExc1.GetObjectData;
+            string f5 = fileLoadExc1.HelpLink;
+            int f6 = fileLoadExc1.HResult*/
+        }
+    }
+}
