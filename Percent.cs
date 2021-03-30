93ccc489279059c33035c07217da70f41c124891
RezaMp <52689106+RezaMpx@users.noreply.github.com>
2021-03-30 19:41:11 +0430

    Update Percent.cs

--- Percent.cs
+++ Percent.cs
@@ -8,17 +8,3 @@
             if (c != a)
             {
                 m = File.ReadAllText("Config.cfg");
-                c = int.Parse(m);
-            }
-            if (a == c)
-            {
-                b = (b / max) * (100);
-                Console.WriteLine(Math.Floor(b) + "%");
-                c = c + f;
-            }
-            File.WriteAllText("Config.cfg", c.ToString());
-        }
-        public double D(int max, int a)
-        {
-            string m = "";
-            double b = a;
