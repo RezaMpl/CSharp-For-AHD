5e6e84bd1c22e96375e48820ff38aaee8fe4d213
RezaMp <52689106+RezaMpx@users.noreply.github.com>
2021-04-27 06:35:47 +0430

    Update Percent.cs

--- Percent.cs
+++ Percent.cs
@@ -23,8 +23,4 @@
             int c = 0;
          
         public void Percent(int max, int f, int a)
-        {
-            string m = "";
-            double b = a;
-            int c = 0;
-         
+        
